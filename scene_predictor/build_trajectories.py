334f7a8a416924acdf953c0d376437be28c8d30c
dhruvmetha <dhruv.metha@gmail.com>
2024-02-23 00:20:48 -0500

    added img as targ

--- scene_predictor/build_trajectories.py
+++ scene_predictor/build_trajectories.py
@@ -4,7 +4,8 @@ import multiprocessing as mp
 from tqdm import tqdm
 from pathlib import Path
 from matplotlib import pyplot as plt
-
+from matplotlib import patches as pch
+from PIL import Image
 
 def build_traj(files, dest_path):
     ctr = 0
@@ -13,27 +14,47 @@ def build_traj(files, dest_path):
             data = np.load(f)
             print(data['input'].shape[0])
             for i in tqdm(range(data['input'].shape[0])):
+                
                 new_data = {
                     'input': data['input'][i],
-                    'll_actions': data['ll_actions'][i],
-                    'actions': data['actions'][i],
+                    # 'll_actions': data['ll_actions'][i],
+                    # 'actions': data['actions'][i],
                     'target': data['target'][i],
                     'target_env': data['target_env'][i],
                     'fsw': data['fsw'][i],
                     'done': data['done'][i],
-
-                    # 'input': self.input_data[env_ids].clone().cpu(), # dof_pos, dof_vel, torques_applied
-                    # 'll_actions': self.ll_actions_data[env_ids].clone().cpu(), # ll_actions
-                    # 'actions': self.actions_data[env_ids].clone().cpu(), # actions
-                    # 'target': self.target_data[env_ids].clone().cpu(), # base_pos, base_ang, base_lin_vel, base_ang_vel
-                    # 'target_env': self.target_env_data[env_ids].clone().cpu(), 
-                    # 'fsw': self.fsw_data[env_ids].clone().cpu(),
-                    # 'done': self.done_data[env_ids].clone().cpu(),
                 }
-            
+
+                target_env = data['target_env'][i]
+                done_idx = data['done'][i].nonzero()[0][-1]
+                obs_img = np.zeros((done_idx, 150, 300, 3))
+                fig, ax = plt.subplots(1, 1, figsize=(3, 1.5), dpi=100)
+                for j in range(done_idx):
+                    plt.cla()
+                    x, y, angle, size_x, size_y = target_env[j, 2:7]
+                    angle = np.rad2deg(angle)
+                    rect = pch.Rectangle(np.array([x, y]) - np.array([size_x, size_y])/2, size_x, size_y, angle=angle, rotation_point='center', facecolor='blue')
+                    ax.add_patch(rect)
+
+                    x, y, angle, size_x, size_y = target_env[j, 11:16]
+                    angle = np.rad2deg(angle)
+                    rect = pch.Rectangle(np.array([x, y]) - np.array([size_x, size_y])/2, size_x, size_y, angle=angle, rotation_point='center', facecolor='red')
+                    ax.add_patch(rect)
+
+                    # set the axis limits
+                    ax.set(xlim=(0.0, 4.0), ylim=(-1, 1))
+                    ax.axis('off')
+
+                    plt.savefig(f'{dest_path}/tmp.png', format='png', )
+                    img = Image.open(f'{dest_path}/tmp.png')
+                    image_array = np.array(img)
+                    obs_img[j] = image_array[:, :, :3]
+                plt.close()
+                new_data['obs_img'] = obs_img       
                 np.savez_compressed(f'{dest_path}/data_{ctr}.npz', **new_data)
                 ctr += 1
-        except:
+        except Exception as e:
+            print(e)
             print(f'missed {ctr}')
             ctr += 1
 
@@ -43,7 +64,7 @@ def main(all_files, dest_path):
     print(len(all_files))
 
     files_per_worker = max(1, int(len(all_files)/num_workers))
-    
+
     workers = []
     # each worker gets a subset of all_files
     for i in range(num_workers):
@@ -83,13 +104,11 @@ if __name__ == '__main__':
     root_traj_path = f'{root_path}/trajectories'
     sub_path = 'iros24_play_feb22/2_obs'
     all_files = glob.glob(f"{root_path}/{sub_path}/*/*/*.npz") # + glob.glob(f"/common/users/dm1487/legged_manipulation_data_store/2_obs/sep16/*lag_6*/*/*.npz")
-
-    print(len(all_files))
     
     small_set = []
     small_set += all_files
     random.shuffle(small_set) 
-    # sub_path = 'iros24_play_feb22/2_obs'
+    sub_path = 'iros24_img_play_feb22/2_obs'
     dest_path = Path(f'{root_traj_path}/{sub_path}/all_data')
     dest_path.mkdir(parents=True, exist_ok=True)
     main(small_set, dest_path)
